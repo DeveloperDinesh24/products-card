fad4aa09fffe764a1b18eaf521363596857d0c2f
Dinesh <1442dinesh6singh5@gmail.com>
2026-01-16 12:39:48 +0530

    feat: add one hard coded product-card

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,20 @@ function App() {
 
   return (
     <>
-      
+      <main className='h-full w-full bg-slate-300 flex justify-between gap-12'>
+        <div className='w-84 h-fit bg-slate-400 border rounded-lg overflow-hidden'>
+          <img 
+          src='https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princess/1.webp' 
+          alt='product-image' 
+          className='w-full border-b' />
+          <div className='p-4 bg-green-300'>
+            <h2 className='text-lg font-semibold'>Name: <span className='font-normal'>Essence Mascara Lask Princess</span></h2>
+            <p className='text-lg font-semibold'>Price: <span className='font-normal'>$9.99</span></p>
+            <p className='text-lg font-semibold'>Category: <span className='font-normal'>Beauty</span></p>
+            <p className='text-lg font-semibold'>Description: <span className='font-normal'>The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects. Achieve dramatic lashes with this long-lasting and cruelty-free formula.</span></p>
+          </div>
+        </div>
+      </main>
     </>
   )
 }
