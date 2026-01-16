5f36eee73476064301ad11138693244b62b58aa2
Dinesh <1442dinesh6singh5@gmail.com>
2026-01-16 14:46:03 +0530

    feat: add search bar

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,12 @@ import ProductCard from './Components/ProductCard'
 
 function App() {
   const [productsData, setProductsData] = useState<ProductData[]>([])
-  // const isProductsDataEmpty = productsData.length === 0 ? true : false
+  const isProductsDataEmpty = productsData.length === 0 ? true : false
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const filteredProducts = productsData.filter((product) =>
+    product.title.toLowerCase().includes(searchTerm.toLowerCase())
+  )
 
   useEffect((): void => {
     fetch('https://dummyjson.com/products')
@@ -16,14 +21,24 @@ function App() {
       })
   }, [])
 
-  // if (!isProductsDataEmpty) {
-  //   console.log(productsData)
-  // }
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setSearchTerm(e.target.value)
+  }
+  if (!isProductsDataEmpty) {
+    console.log(productsData)
+  }
 
   return (
     <div className='w-full min-h-dvh h-full bg-slate-300 flex flex-col gap-16 px-6 py-12'>
+      <input
+        onChange={handleChange}
+        type='text'
+        value={searchTerm}
+        placeholder='Search product...'
+        className='w-72 h-10 py-1 px-2 text-xl bg-slate-400 outline-0 outline-slate-700 focus:outline rounded ml-8'
+      />
       <main className='h-full w-full max-[1536px]:justify-around flex flex-wrap justify-between items-stretch gap-12'>
-        {productsData.map((product: ProductData) => {
+        {filteredProducts.map((product: ProductData) => {
           return <ProductCard key={product.id} product={product} />
         })}
       </main>
