10a8c3a622e709e6a21ef5af4b840692dc2b0618
Dinesh <1442dinesh6singh5@gmail.com>
2026-01-16 12:38:50 +0530

    refactor: add code to fetch products data and store it

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
+interface ProductData {
+  id: number
+  image: string
+  name: string
+  description: string
+  category: string
+  price: number
+}
+
 function App() {
-  return <></>
+  const [productsData, setProductsData] = useState<ProductData[]>([])
+  const isProductsDataEmpty = productsData.length === 0 ? true : false
+
+  useEffect((): void => {
+    fetch('https://dummyjson.com/products')
+      .then((res) => res.json())
+      .then((data): void => setProductsData(() => data.products))
+  }, [])
+
+  if (!isProductsDataEmpty) {
+    console.log(productsData)
+  }
+
+  return (
+    <>
+      
+    </>
+  )
 }
 
 export default App
