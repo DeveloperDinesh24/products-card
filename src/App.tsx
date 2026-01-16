5a6ffe7ac76f5052c699a8bd7d854156c01a6fba
Dinesh <1442dinesh6singh5@gmail.com>
2026-01-16 12:47:31 +0530

    feat: add all product cards using map()

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import './App.css'
 
 interface ProductData {
   id: number
-  image: string
-  name: string
+  images: [string]
+  title: string
   description: string
   category: string
   price: number
@@ -26,19 +26,33 @@ function App() {
 
   return (
     <>
-      <main className='h-full w-full bg-slate-300 flex justify-between gap-12'>
-        <div className='w-84 h-fit bg-slate-400 border rounded-lg overflow-hidden'>
-          <img 
-          src='https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princess/1.webp' 
-          alt='product-image' 
-          className='w-full border-b' />
-          <div className='p-4 bg-green-300'>
-            <h2 className='text-lg font-semibold'>Name: <span className='font-normal'>Essence Mascara Lask Princess</span></h2>
-            <p className='text-lg font-semibold'>Price: <span className='font-normal'>$9.99</span></p>
-            <p className='text-lg font-semibold'>Category: <span className='font-normal'>Beauty</span></p>
-            <p className='text-lg font-semibold'>Description: <span className='font-normal'>The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects. Achieve dramatic lashes with this long-lasting and cruelty-free formula.</span></p>
-          </div>
-        </div>
+      <main className='h-full w-full p-6 bg-slate-300 flex flex-wrap justify-between gap-12'>
+        {productsData.map((product: ProductData) => {
+          return (
+            <div
+              key={product.id}
+              className='w-84 h-fit bg-slate-400 border rounded-lg overflow-hidden'
+            >
+              <img src={product.images[0]} className='w-full border-b' />
+              <div className='p-4 bg-green-300'>
+                <h2 className='text-lg font-semibold'>
+                  Name: <span className='font-normal'>{product.title}</span>
+                </h2>
+                <p className='text-lg font-semibold'>
+                  Price: <span className='font-normal'>${product.price}</span>
+                </p>
+                <p className='text-lg font-semibold'>
+                  Category:{' '}
+                  <span className='font-normal'>{product.category}</span>
+                </p>
+                <p className='text-lg font-semibold'>
+                  Description:{' '}
+                  <span className='font-normal'>{product.description}</span>
+                </p>
+              </div>
+            </div>
+          )
+        })}
       </main>
     </>
   )
